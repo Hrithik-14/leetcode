7f827ab548f547f7738dab827e344f092629ccf2
Hrithik-14 <hrithikelayur12@gmail.com>
2025-05-24 11:16:31 +0530

    Time: 0 ms (100%), Space: 52 MB (98.77%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+   let str = s.trim().split(" ")
+  let word = str[str.length - 1] 
+  return word.length
+};
+lengthOfLastWord("Hello World");
+lengthOfLastWord("")
