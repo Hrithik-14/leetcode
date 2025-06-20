a974496001b75e0dfe8e9ca5b57de1a69f269ccf
Hrithik-14 <hrithikelayur12@gmail.com>
2025-06-20 09:59:12 +0530

    Time: 1 ms (50.53%), Space: 55.6 MB (40%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+
+    let set = new Set(nums)
+
+    while(set.has(original)){
+
+        original *= 2
+    }
+    return original
+    
+};
