5faa122ae58c8788ec7cf42b10ad31e93374477a
Hrithik-14 <hrithikelayur12@gmail.com>
2025-07-03 10:17:20 +0530

    Time: 2 ms (53.09%), Space: 58.2 MB (26.29%) - LeetHub

---
+++ 2977-check-if-a-string-is-an-acronym-of-words/2977-check-if-a-string-is-an-acronym-of-words.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+var isAcronym = function(words, s) {
+
+
+    return s === words.map(word => word[0]).join('');
+    
+};
